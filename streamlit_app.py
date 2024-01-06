b701c56578cd8750826d61e821704033cf8bb32f
Kota Mori <kota.mori@teradata.com>
2024-01-07 02:59:41 +0900

    add more skill filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,35 @@ class const:
     SELECT DISTINCT spec FROM specs ORDER BY 1
     """).values[:,0]
 
+    skill_attrs = [c for c in get_query("""
+    SELECT * FROM skills
+    """).columns if c[0] < "a" or c[0] >= "z"]
+
+    skill_targets = get_query("""
+    SELECT DISTINCT target FROM skills ORDER BY 1
+    """).values[:,0]
+
+    power_max = get_query("""
+    SELECT MAX(pw) FROM skills
+    """).values[0,0]
+
+    power_min = get_query("""
+    SELECT MIN(pw) FROM skills
+    """).values[0,0]
+
+    hit_max = get_query("""
+    SELECT MAX(hit) FROM skills
+    """).values[0,0]
+
+    hit_min = get_query("""
+    SELECT MIN(hit) FROM skills
+    """).values[0,0]
+
+    skill_percents = get_query("""
+    SELECT DISTINCT CASE WHEN percent IS NULL THEN 'NA' ELSE CAST(percent AS TEXT) END AS p FROM skills ORDER BY percent NULLS LAST
+    """).values[:,0]
+
+
     table_width = 1000
     table_height = 700
 
@@ -67,7 +96,20 @@ def main():
         select_skill = col1.multiselect("技1", const.skills)
         select_skill2 = col2.multiselect("技2", const.skills)
         select_spec = st.multiselect("特性", const.specs)
-
+        st.markdown("----")
+        
+        col1, col2 = st.columns([1, 1])
+        select_skill_class = col1.multiselect("種別", ["物理", "特殊", "変化"])
+        select_skill_contact = col2.multiselect("接触有無", ["接触", "非接触"])
+        col1, col2 = st.columns([7, 5])
+        select_skill_attr = col1.multiselect("技特徴", const.skill_attrs)
+        select_skill_target = col2.multiselect("対象", const.skill_targets)
+        #select_skill_percent = col2.multiselect("確率", const.skill_percents)
+        col1, col2 = st.columns([1, 1])
+        slider_skill_power = col1.slider("威力", min_value=const.power_min, max_value=const.power_max, 
+                                         value=[const.power_min, const.power_max], step=5)
+        slider_skill_hit = col2.slider("命中率", min_value=const.hit_min, max_value=const.hit_max,
+                                       value=[const.hit_min, const.hit_max], step=5)
         text_skill = st.text_input("技検索")
         text_spec = st.text_input("特性検索")
         text_item = st.text_input("持ち物検索")
@@ -120,20 +162,43 @@ def main():
         filters = []
         if text_skill:
             filters.append(f"k.skill LIKE '%{text_skill}%' OR k.desc LIKE '%{text_skill}%'")
+        if select_skill_class:
+            filters.append(f"k.class IN {tuple(select_skill_class + ['foo'])}")
+        if select_skill_target:
+            filters.append(f"k.target IN {tuple(select_skill_target + ['foo'])}")
+        if select_skill_contact:
+            vals = [0 if c == "非接触" else 1 if c == "接触" else 99 for c in select_skill_contact] + [99]
+            filters.append(f"k.contact IN {tuple(vals)}")
         if select_skill or select_skill2:
             filters.append(f"k.skill IN {tuple(select_skill + select_skill2 + ['foo'])} OR desc IN {tuple(select_skill + select_skill2 + ['foo'])}")
         if select_pokemon_name:
             filters.append(f"mk.uname IN {tuple(select_pokemon_name + ['foo'])}")
-        filter = " AND ".join(f"( {f} )" for f in filters) if filters else "1"
+        if select_skill_attr:
+            for attr in select_skill_attr:
+                filters.append(f"k.{attr}")
+        filters.append("k.pw BETWEEN {} AND {}".format(*slider_skill_power))
+        filters.append("k.hit BETWEEN {} AND {}".format(*slider_skill_hit))
+
+        filter_ = " AND ".join(f"( {f} )" for f in filters) if filters else "1"
         df = get_query(f"""
         SELECT DISTINCT
           {'mk.uname,' if select_pokemon_name else ''}
-          k.*
+           k.skill
+          ,k.type
+          ,k.class
+          ,k.target
+          ,k.pw
+          ,k.hit
+          ,k.pp
+          ,k.attr
+          ,k.percent
+          ,k.desc
+          ,k.url
         FROM
           skills AS k
           {'INNER JOIN monster_skills AS mk ON k.skill = mk.skill' if select_pokemon_name else ''}
-        WHERE {filter}
-        ORDER BY k.skill
+        WHERE {filter_}
+        ORDER BY {'mk.skill_order,' if select_pokemon_name else ''} k.skill
         """)
         st.data_editor(df, hide_index=True, width=const.table_width, height=const.table_height)
 
